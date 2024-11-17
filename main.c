fe01ac8b7eaa908c5ae8f432c2dc8173348bd1bd
Patrick Atef <patrickatefffa22@gmail.com>
2024-11-17 17:29:31 +0200

    Add Modulus

--- main.c
+++ main.c
@@ -1,39 +1,31 @@
-#include <stdio.h>
-#include <stdint.h>
+#include<stdio.h>
+#include<stdint.h>
 
+void Modulus(int32_t x, int32_t y);
 
-int32_t sum(int32_t num1,int32_t num2);
-
-int32_t subtraction(int32_t x, int32_t y);
-
-
-int main(void)
+int main ()
 {
-    int32_t x = 0, y = 0;
-    int32_t Res = 0;
 
-
+    int32_t x = 0, y =0,result=0;
     printf("Enter two numbers: ");
     scanf("%d %d", &x, &y);
-	
-    Res = sum(x,y);
-    printf("SUM = %d\n",Res);
-
-    Res = subtraction(x, y); 
-    printf("Subtraction Result: %d\n", Res);
-
-
-    return 0;
+    Modulus(x,y);
 }
 
 
-int32_t sum(int32_t num1,int32_t num2)
+void Modulus(int32_t x, int32_t y)
 {
-    return num1 + num2;
+    int Result=0;
+
+    if (y==0)
+    {
+       printf("division by zero is not allowed -__-");
+    }
+
+    else 
+    {
+        Result = x%y;
+    
+        printf("%d",Result);
+    }
 }
-
-
-int32_t subtraction(int32_t x, int32_t y) {
-    return x - y;
-}
-
