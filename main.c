8b963b60593d4acd54a554ab2d8e5ad37480fe96
Musa-Mahmoud <mosa.mahmoud87@gmail.com>
2024-11-17 16:53:42 +0200

    Feature: Add divide function

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdint.h>
+
+int32_t Divide(int32_t Dividend, int32_t Divisor);
 
 int main(void)
 {
+    int32_t x = 0, y = 0;
+    int32_t Res = 0;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    Res = Divide(x, y);
+    printf("%d / %d = %d\n", x, y, Res);
+
     return 0;
+}
+
+int32_t Divide(int32_t Dividend, int32_t Divisor)
+{
+    if (Divisor == 0)
+    {
+        return INT32_MIN;
+    }
+    return (Dividend / Divisor);
 }
